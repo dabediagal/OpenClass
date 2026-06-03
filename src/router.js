38545c9679577e822280bde34d25374b54c1ba73
Daniela <daniela.diaz71@educa.madrid.org>
2026-06-03 18:30:14 +0200

    correccion router

--- src/router.js
+++ src/router.js
@@ -19,11 +19,11 @@ router.post('/user/new', (req, res) => {
     const user = new User(req.body.name, req.body.type);
 	VirtualClass.addUser(user.id, user);
 
-    res.redirect('/new_user.html');
+	res.redirect('/new_user.html');
 });
 
 router.post('/subject/new', (req, res) => {
-    const subject = new Subject(req.body.name);
-	VirtualClass.addSubject(subject.id, subject);
-    res.redirect('/new_subject.html');
+	const subject = new Subject(req.body.name);
+	VirtualClass.addSubject(subject);
+	res.redirect('/new_subject.html');
 });
