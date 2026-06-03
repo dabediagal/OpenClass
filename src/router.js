741cadd2e4a9c1408cf969ba2951d2932c717810
Sonia <sonia.alvachez@gmail.com>
2026-06-03 18:15:01 +0200

    Cambios de Sonia

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
-import Subject from './subject.js';
-import User from './user.js';
-import VirtualClass from './virtual_class.js';
+import { Subject } from './subject.js';
+import { User } from './user.js';
+import { VirtualClass } from './virtual_class.js';
 
 import multer from 'multer';
 import fs from 'node:fs/promises';
@@ -15,16 +15,16 @@ router.get('/', (req, res) => {
 	res.render('index', { subjects: subjects });
 });
 
-router.get('/user/new', (req, res) => {
+router.post('/user/new', (req, res) => {
     const user = new User(req.body.name, req.body.type);
 	VirtualClass.addUser(user.id, user);
 
-    res.redirect('/user/new');
+    res.redirect('/new_user.html');
 });
 
-router.get('/subject/new', (req, res) => {
+router.post('/subject/new', (req, res) => {
     const subject = new Subject(req.body.name);
 	VirtualClass.addSubject(subject.id, subject);
-    res.redirect('/subject/new');
+    res.redirect('/new_subject.html');
 });
 
