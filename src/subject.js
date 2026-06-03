84cffcf3e9037ad849528b60d0982a500b7b3844
Daniela <daniela.diaz71@educa.madrid.org>
2026-06-03 17:48:02 +0200

    subject formateado

--- src/subject.js
+++ src/subject.js
@@ -1,37 +1,38 @@
 import { User } from './user.js';
-import { VirtualClass } from './virtual_class.js'
-export class Subject{
-    static counter=0;
-    constructor(name){
-        Subject.counter++;
-        this.id=Subject.counter;
-        this.name=name;
-        this.teachers=[]; //RECIBE IDS SÓLO
-        this.students=[];
-    }
-    getTeachers(){
-        let teachersFullInfo=[]; //AQUI GUARDARE LOS TEACHERS COMPLETOS, NOT ONLY IDS
-        for(teacher of this.teachers){
-            teachersFullInfo.push(VirtualClass.getUser(teacher));
-        }
-        return teachersFullInfo;
-    }
-    getStudents(){
-        let studentsFullInfo=[];
-        for(student of this.students){
-            studentsFullInfo.push(VirtualClass.getUser(student));
-        }
-        return studentsFullInfo;
-    }
-    
-    addUser(id){ //se une mediante el id interno que selecciona con un desplegable a la hora de añadir usuario
-        let user= VirtualClass.getUser(id);
-        if(user.type==='teacher'){
-            this.teachers.add(id);
-        }else{
-            this.students.add(id);
-        }
-    }
-    
-    //deleteUser
+import { VirtualClass } from './virtual_class.js';
+export class Subject {
+	static counter = 0;
+	constructor(name) {
+		Subject.counter++;
+		this.id = Subject.counter;
+		this.name = name;
+		this.teachers = []; //RECIBE IDS SÓLO
+		this.students = [];
+	}
+	getTeachers() {
+		let teachersFullInfo = []; //AQUI GUARDARE LOS TEACHERS COMPLETOS, NOT ONLY IDS
+		for (teacher of this.teachers) {
+			teachersFullInfo.push(VirtualClass.getUser(teacher));
+		}
+		return teachersFullInfo;
+	}
+	getStudents() {
+		let studentsFullInfo = [];
+		for (student of this.students) {
+			studentsFullInfo.push(VirtualClass.getUser(student));
+		}
+		return studentsFullInfo;
+	}
+
+	addUser(id) {
+		//se une mediante el id interno que selecciona con un desplegable a la hora de añadir usuario
+		let user = VirtualClass.getUser(id);
+		if (user.type === 'teacher') {
+			this.teachers.add(id);
+		} else {
+			this.students.add(id);
+		}
+	}
+
+	//deleteUser
 }
