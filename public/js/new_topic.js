0da8df4494e55476f33f52b4acb147052d1c5e7d
Sonia <sonia.alvachez@gmail.com>
2026-06-09 13:05:56 +0200

    Refactor new topic creation logic to improve success handling and user feedback

--- public/js/new_topic.js
+++ public/js/new_topic.js
@@ -7,16 +7,13 @@ async function newTopic(event){
 		body: formData,
 	});
 
-    if(response.ok){
-        const accept = confirm('¡El tema ha sido creado con exito! ¿Quieres seguir creando temas?');
-        if(accept){
-            document.getElementById('newTopic').reset();
-        }else{
-            window.location = `/subject/${SUBJECT_ID}`; //al index pq no hay ventana de subjects 
-        }
-            
-    }else{
-        alert('Ha ocurrido un error.');
+    const result = await response.json();
+
+    if (result.valid) {
+        alert('¡El tema ha sido creado con éxito!');
+        window.location.reload();
+    } else {
+        alert(`Error: ${result.message}`);
     }
 	
 }
