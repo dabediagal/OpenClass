b16e053e289e86fb6963398dffdd2399baa08605
Sonia <sonia.alvachez@gmail.com>
2026-06-10 13:15:33 +0200

    Add user management functions for deletion and editing

--- public/js/show_users.js
+++ public/js/show_users.js
@@ -1,3 +1,5 @@
+import { VirtualClass } from './src/models/virtual_class.js';
+
 async function deleteUser(userId) {
 	const accept = confirm('¿Estás seguro de que quieres eliminar este usuario?');
 	if (!accept) {
@@ -11,5 +13,17 @@ async function deleteUser(userId) {
 		window.location = '/users';
 	} else {
 		alert(`Error: ${result.message}`);
+	}
+}
+
+async function editUser(userId){
+	const teacher=document.getElementById('teacher');
+	const student=document.getElementById('student');
+
+	const user=VirtualClass.getUser(userId);
+	if(user.type==='teacher'){
+		teacher.innerHTML='';
+	}else{
+		student.innerHTML='';
 	}
 }
